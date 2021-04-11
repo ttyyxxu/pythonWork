dcdd5a4ea5f975605a148ec443a0db847cea4c4c
ttyyxxu <ttyyxxu>
2021-04-11 10:45:14 +0800

    d

--- Nowcoder/24cal.py
+++ Nowcoder/24cal.py
@@ -1,9 +1,15 @@
 import re
 import itertools
 
-N1 = 0
-N2 = 0
-N3 = 0
-N4 = 0
+numbers = ['1','3','4','5']
+def calc24(numbers):
+    for num in itertools.permutations(numbers):
+        for cal in itertools.product(['+','-','*','/'],repeat=3):
+            expr = '(('+num[0]+cal[0]+num[1]+')'+cal[1]+num[2]+')'+cal[2]+num[3]
+            if eval(expr) == 24:
+                return expr
 
-print(list(itertools.permutations([1,2,3,4])))
+    return None
+
+print(calc24(numbers))
+
