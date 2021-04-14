5c6d420e507bb41501d0fdcfb37e0e3ad775dcbd
ttyyxxu <ttyyxxu>
2021-04-14 21:58:02 +0800

    d

--- Nowcoder/24cal.py
+++ Nowcoder/24cal.py
@@ -1,19 +1,21 @@
 import re
 import itertools
 
-numbers = ['106','100','204','200']
+numbers = ['5','5','5','1']
 def calc24(numbers):
     for num in itertools.permutations(numbers):
         for ops in itertools.product(['+','-','*','/'],repeat=3):
             exp1 = '({0}{4}{1}){5}({2}{6}{3})'.format(*num, *ops)
-            expr = '(('+num[0]+cal[0]+num[1]+')'+cal[1]+num[2]+')'+cal[2]+num[3]
-            try:
-                if eval(expr) == 24:
-                    return expr
-            except ZeroDivisionError:
-                print("i got zero devision, pass")
-                continue
-
+            exp2 = '(({0}{4}{1}){5}{2}){6}{3}'.format(*num, *ops)
+            exp3 = '{0}{4}({1}{5}({2}{6}{3}))'.format(*num, *ops)
+            # expr = '(('+num[0]+cal[0]+num[1]+')'+cal[1]+num[2]+')'+cal[2]+num[3]
+            for expr in exp1,exp2,exp3:
+                try:
+                    if abs(eval(expr) - 24) < 0.000001:
+                        return expr
+                except ZeroDivisionError:
+                    # print("i got zero devision, pass")
+                    continue
     return None
 
 print(calc24(numbers))
