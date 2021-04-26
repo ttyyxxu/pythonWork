0884768fc0126413da84f317f50d055fae95e3c4
ttyyxxu <mcdyess_dunker@hotmail.com>
2021-04-26 17:44:50 +0800

    commit

---
+++ search.py
@@ -0,0 +1,17 @@
+import argparse
+import getpass
+import os
+import subprocess
+import shutil
+
+# parser = argparse.ArgumentParser(description='search files')
+# parser.add_argument(dest='filenames')
+
+# user = getpass.getuser()
+# passwd = getpass.getpass()
+# print(user,passwd,"ddd")
+
+# output = subprocess.check_output(['date']).decode('utf-8')
+# print(output)
+
+shutil.copy2('ps2.py','ps222.py')
