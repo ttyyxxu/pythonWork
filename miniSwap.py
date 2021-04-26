f1257bfd897075c53cfe0e1c061c01401ebfcc50
ttyyxxu <ttyyxxu>
2021-04-26 22:06:56 +0800

    k

--- miniSwap.py
+++ miniSwap.py
@@ -1,3 +1,11 @@
 def miniswap(arr):
-    for i in enumerate(arr):
-        if arr[i] !=
+    steps = 0
+    for i,num in enumerate(arr):
+        if num != i+1:
+            arr[arr.index(i+1)], arr[i] = arr[i], arr[arr.index(i+1)]
+            steps += 1
+            print(steps,arr)
+
+miniswap([7,1,3,2,4,5,6])
+
+miniswap([2,3,4,1,5])
