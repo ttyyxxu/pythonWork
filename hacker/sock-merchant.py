43a8c4458c77672da5b6d0c1e368490faddfaeb1
ttyyxxu <ttyyxxu>
2021-04-28 20:14:09 +0800

    k

--- hacker/sock-merchant.py
+++ hacker/sock-merchant.py
@@ -25,7 +25,7 @@ def sockMerchant(n, ar):
 # The function accepts INTEGER_ARRAY q as parameter.
 #
 
-def minimumBribes(q):
+def minimumBribes_old(q):
     bribes = 0
     for i in range(len(q)):
         climb = q[i] - (i+1)
@@ -35,6 +35,21 @@ def minimumBribes(q):
         elif climb > 0:
             bribes += climb
     print(int(bribes))
+
+def minimumBribes(q):
+    L = len(q)
+    bribes = 0
+    for pos,i in enumerate(q):
+        climb = 0
+        for j in range(pos+1,L):
+            if i > q[j]:
+                climb +=1
+            if climb > 2:
+                print("Too chaotic")
+                return
+        bribes += climb
+    print(bribes)
+
 
 minimumBribes([5,1,2,3,7,8,6,4])
 minimumBribes([1,2,5,3,7,8,6,4])
