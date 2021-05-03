5fba7548a71c8fa854e4da73d08a08e987fbbde2
ttyyxxu <mcdyess_dunker@hotmail.com>
2021-05-03 22:04:19 +0800

    commit

--- hacker/string.py
+++ hacker/string.py
@@ -2,17 +2,16 @@ def makeAnagram(a,b):
     delete_needed = 0
     char_checked = []
     for s in a:
-        if s in char_checked:
-            continue
-        else:
+        if s not in char_checked:
             delete_needed += abs(a.count(s) - b.count(s))
             char_checked.append(s)
 
     for t in b:
         if t not in char_checked:
             delete_needed += abs(a.count(t) - b.count(t))
+            char_checked.append(t)
 
     return delete_needed
 
 print(makeAnagram('cde','dcf'))
-print(makeAnagram('cde','abc'))
+print(makeAnagram('fcrxzwscanmligyxyvym','jxwtrhvujlmrpdoqbisbwhmgpmeoke'))
