3113fb8818ad0466a967d249508c2fbae598d4a2
ttyyxxu <ttyyxxu>
2021-07-05 22:09:42 +0800

    7-5

---
+++ hacker/sorting.py
@@ -0,0 +1,18 @@
+import statistics
+def activityNotifications(expenditure, d):
+    notice = 0
+    ll = len(expenditure)
+    for i in range(d,ll):
+        if expenditure[i] >= 2 * statistics.median(expenditure[i-d:i]):
+            print(expenditure[i], expenditure[i-d:i], statistics.median(expenditure[i-d:i]))
+            notice+=1
+    return notice
+
+activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5],5)
+
+'''
+STDIN               Function
+-----               --------
+9 5                 expenditure[] size n =9, d = 5
+2 3 4 2 3 6 8 4 5   expenditure = [2, 3, 4, 2, 3, 6, 8, 4, 5]
+'''
